08a6e42c3965d150c315116bef4fc3cf42d47f18
mrfrolk <ekaterita20@gmail.com>
2025-03-29 14:17:02 +0500

    1

---
+++ lib/features/tasks/domain/usecases/category/get_by_id.dart
@@ -0,0 +1,12 @@
+import '../../repositories/category_repository.dart';
+import '../../entities/category_entity.dart';
+
+class GetCategoryByIdUseCase {
+  final CategoryRepository _repository;
+
+  GetCategoryByIdUseCase(this._repository);
+
+  Future<CategoryEntity?> call(int id) {
+    return _repository.getCategoryById(id);
+  }
+}
